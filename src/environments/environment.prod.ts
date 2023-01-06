d9a8253bf513bd82c8eb57732dda7b5dadf2a224
Nathaniel Campbell <nathanielcampbell2002@gmail.com>
2023-01-06 00:04:39 -0500

    Changed API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'https://amber-personal-project.herokuapp.com/api/v1'
+    API_URL: 'https://personal-project-api.vercel.app/api/v1'
 };
